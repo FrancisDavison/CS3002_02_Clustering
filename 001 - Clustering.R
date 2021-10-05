9817ca7127be293f331fc044e1a66ae4da907297
Francis Davison <francis.nj.davison@gmail.com>
2021-10-05 16:02:50 +0100

    continued working on lab

--- 001 - Clustering.R
+++ 001 - Clustering.R
@@ -1,33 +1,33 @@
-mydata = read.csv('C:\\Users\\Picard\\Dropbox\\Github\\CS3002_02_Clustering\\spaeth_02.csv', sep=",")
+mydata = read.csv('C:\\Users\\Picard\\Dropbox\\Github\\CS3002_02_Clustering\\seeds_dataset.csv', sep=",");
 
-plot(mydata)
+plot(mydata);
 
 # optional: Prepare Data
-mydata = na.omit(mydata) # deletion of missing data
-mydata = scale(mydata) # standardize variables
+mydata = na.omit(mydata); # deletion of missing data
+#mydata = scale(mydata); # standardize variables
 
-d <- dist(mydata, method = "euclidean") # distance matrix
+d <- dist(mydata, method = "euclidian"); # distance matrix
 
-fit <- hclust(d, method="average")
-plot(fit) # display dendogram
+fit <- hclust(d, method="average");
+plot(fit); # display dendogram
 
-Hgroups <- cutree(fit, k=5) # cut tree into 5 clusters
+Hgroups <- cutree(fit, k=2); # cut tree into 5 clusters
 
-rect.hclust(fit, k=5, border="red")
+rect.hclust(fit, k=2, border="red");
 
-plot(mydata, col=Hgroups)
+plot(mydata, col=Hgroups);
 
 #Kmeans:
 
-fit <- kmeans(mydata, 5) # 5 cluster solution
+fit <- kmeans(mydata, 2); # 5 cluster solution
 
-aggregate(mydata,by=list(fit$cluster),FUN=mean)
+aggregate(mydata,by=list(fit$cluster),FUN=mean);
 
-Kgroups = fit$cluster
+Kgroups = fit$cluster;
 
-plot(mydata, col=Kgroups)
+plot(mydata, col=Kgroups);
 
-source("C:\\Users\\Picard\\Dropbox\\Github\\CS3002_02_Clustering\\WK_R.r")
+source("C:\\Users\\Picard\\Dropbox\\Github\\CS3002_02_Clustering\\WK_R.r");
 
-wk = WK_R(Kgroups, Hgroups)
-
+wk = WK_R(Kgroups, Hgroups);
+print(wk)
