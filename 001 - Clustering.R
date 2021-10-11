864f819a7d9e05800a9687a8a3f52f2b2fb28c20
FrancisDavison <francis.nj.davison@gmail.com>
2021-10-11 21:23:15 +0100

    lab complete

--- 001 - Clustering.R
+++ 001 - Clustering.R
@@ -1,13 +1,17 @@
 mydata = read.csv('C:\\Users\\Picard\\Dropbox\\Other Folders\\LABS\\Year 3\\CS3002\\002 - Clustering (Assessed)\\seeds_dataset.csv') #imports the data from seeds_dataset.csv', sep=",")
 source("C:\\Users\\Picard\\Dropbox\\Other Folders\\LABS\\Year 3\\CS3002\\002 - Clustering (Assessed)\\WK_R.R") #imports Weighted Kappa code to use as source
 
-linkage_method="average" #specifies linkage method for hierarchical clustering
-for(k in 2:10)
+min=2 #defines minimum value of k, cannot be less than 2
+max=20 #defines maximum value of k
+
+linkage_measure="average" #specifies linkage method for hierarchical clustering
+wk_store <- data.frame()
+for(k in min:max)
 {
   #Hierarchical Clustering
   d <- dist(mydata, method="euclidian") #distance matrix
   #Average Linkage
-  fit <- hclust(d, method=linkage_method)
+  fit <- hclust(d, method=linkage_measure)
   plot(fit) #displays dendrogram
   Hgroups <- cutree(fit, k)
   rect.hclust(fit, k, border="pink")
@@ -20,4 +24,7 @@ for(k in 2:10)
   plot(mydata, col=Kgroups, main=bquote("Kmeans, k="~.(k))) #plots scatterplot of mydata with Kgroups_n to colour code
   
   wk = WK_R(Kgroups, Hgroups)
+  wk_temp <- data.frame(k,wk)
+  wk_store <- rbind(wk_store, wk_temp)
 }
+plot(wk_store, main="wk against k") #plots the different wk values against their respective k value
