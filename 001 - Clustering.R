3ce39b27bba96d3f890a93bf9d36cff36e554e64
FrancisDavison <francis.nj.davison@gmail.com>
2021-10-11 20:35:12 +0100

    added up to date files

--- 001 - Clustering.R
+++ 001 - Clustering.R
@@ -1,33 +1,22 @@
-mydata = read.csv('C:\\Users\\Picard\\Dropbox\\Github\\CS3002_02_Clustering\\seeds_dataset.csv', sep=",");
+mydata = read.csv('C:\\Users\\Picard\\Dropbox\\Other Folders\\LABS\\Year 3\\CS3002\\002 - Clustering (Assessed)\\seeds_dataset.csv') #imports the data from seeds_dataset.csv', sep=",")
+source("C:\\Users\\Picard\\Dropbox\\Other Folders\\LABS\\Year 3\\CS3002\\002 - Clustering (Assessed)\\WK_R.R") #imports Weighted Kappa code to use as source
 
-plot(mydata);
+linkage_method="average" #specifies linkage method for hierarchical clustering
+k=5 #determines the value of k for hierarchical and kmeans clustering
 
-# optional: Prepare Data
-mydata = na.omit(mydata); # deletion of missing data
-#mydata = scale(mydata); # standardize variables
+#Hierarchical Clustering
+d <- dist(mydata, method="euclidian") #distance matrix
+#Average Linkage
+fit <- hclust(d, method=linkage_method)
+plot(fit) #displays dendrogram
+Hgroups <- cutree(fit, k)
+rect.hclust(fit, k, border="pink")
+plot(mydata, col=Hgroups, main=bquote("Average Linkage, k="~.(k)))
 
-d <- dist(mydata, method = "euclidian"); # distance matrix
+#Kmeans Clustering:
+fit<- kmeans(mydata, k)
+aggregate(mydata,by=list(fit$cluster),FUN=mean)
+Kgroups = fit$cluster #assigns value to Kgroups
+plot(mydata, col=Kgroups, main=bquote("Kmeans, k="~.(k))) #plots scatterplot of mydata with Kgroups_n to colour code
 
-fit <- hclust(d, method="average");
-plot(fit); # display dendogram
-
-Hgroups <- cutree(fit, k=2); # cut tree into 5 clusters
-
-rect.hclust(fit, k=2, border="red");
-
-plot(mydata, col=Hgroups);
-
-#Kmeans:
-
-fit <- kmeans(mydata, 2); # 5 cluster solution
-
-aggregate(mydata,by=list(fit$cluster),FUN=mean);
-
-Kgroups = fit$cluster;
-
-plot(mydata, col=Kgroups);
-
-source("C:\\Users\\Picard\\Dropbox\\Github\\CS3002_02_Clustering\\WK_R.r");
-
-wk = WK_R(Kgroups, Hgroups);
-print(wk)
+wk = WK_R(Kgroups, Hgroups)
