67762cb895d75f7980c5911d5708ffed2c87e332
FrancisDavison <francis.nj.davison@gmail.com>
2021-10-12 17:02:17 +0100

    old files replaced, assessed task almost complete

--- 001 - Clustering.R
+++ 001 - Clustering.R
@@ -1,27 +1,45 @@
 mydata = read.csv('.\\seeds_dataset.csv') #imports the data from seeds_dataset.csv
+truedata = read.csv('.\\seeds_real.csv', header=F)
 source(".\\WK_R.R") #imports Weighted Kappa code to use as source
 min=2 #defines minimum value of k, cannot be less than 2
 max=8 #defines maximum value of k
+wk_store_km <- data.frame()
+wk_store_ha <- data.frame()
+wk_store_hc <- data.frame()
+wk_store_hs <- data.frame()
 
-linkage_measure="average" #specifies linkage method for hierarchical clustering
-wk_store <- data.frame() #creates blank dataframe for storing wk values
-for(k in min:max) #for loop runs hierarchical and kmeans clustering for values of k in range min:max
+
+
+#kmeans
+for(k in min:max)
 {
-  ##Hierarchical Clustering
-  d <- dist(mydata, method="euclidian") #generate distance matrix of euclidian values between datapoints
-  fit <- hclust(d, method=linkage_measure) #performs clustering with linkage measure defined above
-  plot(fit) #plots dendrogram
-  Hgroups <- cutree(fit, k) #cuts dendrogram into k clusters
-  rect.hclust(fit, k, border="pink") #places pink borders to display k clusters
-  plot(mydata, col=Hgroups, main=bquote("hierarchical clustering," ~.(linkage_measure)~"Linkage, k="~.(k))) #plots scatterplot with colour coded clusters
-  
-  ##Kmeans Clustering:
   fit<- kmeans(mydata, k) #calculates kmeans for k clusters
   aggregate(mydata,by=list(fit$cluster),FUN=mean) #gets statistics on clusters using mean value for each variable
   Kgroups = fit$cluster #assigns value to Kgroups
   plot(mydata, col=Kgroups, main=bquote("Kmeans, k="~.(k))) #plots scatterplot of mydata with Kgroups_n to colour code
-  
-  wk = WK_R(Kgroups, Hgroups) #sets wk value to the value returned by WK_R code
-  wk_store <- rbind(wk_store, (wk_temp <- data.frame(k,wk))) #creates wk_temp dataframe, adds k and wk to wk_temp, binds wk_temp to bottom of wk_store
+  wk_km = WK_R(Kgroups, truedata$V1) #sets wk value to the value returned by WK_R code
+  wk_store_km <- rbind(wk_store_km, (wk_temp <- data.frame(k,wk_km))) #creates wk_temp dataframe, adds k and wk to wk_temp, binds wk_temp to bottom of wk_store
 }
-plot(wk_store, main="wk against k") #plots the different wk values against their respective k value
+
+#hierarchical
+for(k in min:max)
+{
+  d <- dist(mydata, method="euclidian") #generate distance matrix of euclidian values between datapoints
+  fit <- hclust(d, method="average") #performs clustering with linkage measure defined above
+  plot(fit) #plots dendrogram
+  Hgroups <- cutree(fit, k) #cuts dendrogram into k clusters
+  rect.hclust(fit, k, border="pink") #places pink borders to display k clusters
+  plot(mydata, col=Hgroups, main=bquote("hierarchical clustering, Average Linkage, k="~.(k))) #plots scatterplot with colour coded clusters
+  wk_ha = WK_R(Kgroups, truedata$V1) #sets wk value to the value returned by WK_R code
+  wk_store_ha <- rbind(wk_store_ha, (wk_temp <- data.frame(k,wk_ha))) #creates wk_temp dataframe, adds k and wk to wk_temp, binds wk_temp to bottom of wk_store
+}
+
+
+
+
+
+
+
+
+plot(wk_store_km, main="wk_km against k") #plots the different wk values against their respective k value
+plot(wk_store_ha, main="wk_km against k") #plots the different wk values against their respective k value
